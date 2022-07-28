dd3fc3a23d10ac1d47cc96341931cddefe7d0272
Nitin Vaghani <nitinvaghani34@gmail.com>
2022-07-28 19:12:06 +0530

    install-mongodb-compass.sh
    How to install mongodb compass

---
+++ install-mongodb-compass.sh
@@ -0,0 +1,11 @@
+#Download MongoDB Compass
+
+wget https://downloads.mongodb.com/compass/mongodb-compass_1.29.5_amd64.deb
+
+#Install MongoDB Compass
+
+sudo dpkg -i mongodb-compass_1.29.5_amd64.deb
+
+#Start MongoDB Compass
+
+mongodb-compass
